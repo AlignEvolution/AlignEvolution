28a30a6840f95f796082140dd9763aa8f3de8905
AlignEvolution <drdarwinfalcao1@gmail.com>
2025-12-15 13:18:41 -0400

    Redirect to login page on Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <main style={{ padding: 40 }}>
-      <h1>Align Evolution</h1>
-      <p>Portal de casos ortodônticos</p>
-    </main>
-  )
+  redirect("/login");
 }
